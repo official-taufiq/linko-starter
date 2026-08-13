ce99b29e43278f516c76253094a3705be674116c
official-taufiq <official.taufiqrahman@gmail.com>
2026-08-13 11:36:03 +0530

    logger cleanup

--- main.go
+++ main.go
@@ -15,8 +15,6 @@ import (
 	"boot.dev/linko/internal/store"
 )
 
-// var Logger = log.New(os.Stderr, "DEBUG: ", log.LstdFlags)
-
 func main() {
 
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
@@ -33,29 +31,24 @@ func main() {
 
 func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir string) int {
 
-	var initializeLogger *log.Logger
-
-	logEnv, exists := os.LookupEnv("LINKO_LOG_FILE")
-	if exists {
-		file, err := os.OpenFile(logEnv, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-		if err != nil {
-
-			fmt.Fprintf(os.Stderr, "Failed to open log file: %v", err)
-		}
-		defer file.Close()
-		bufferedFile := bufio.NewWriterSize(file, 8192)
-		multiWriter := io.MultiWriter(bufferedFile, os.Stderr)
-		initializeLogger = log.New(multiWriter, "", log.LstdFlags)
-	} else {
-		initializeLogger = log.New(os.Stderr, "", log.LstdFlags)
+	logger, closeLogger, err := initializeLogger(os.Getenv("LINKO_LOG_FILE"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to initialize Logger: %v\n", err)
 	}
 
-	st, err := store.New(dataDir, initializeLogger)
+	defer func() {
+		err := closeLogger()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to close Logger: %v\n", err)
+		}
+	}()
+
+	st, err := store.New(dataDir, logger)
 	if err != nil {
-		initializeLogger.Printf("failed to create store: %v", err)
+		logger.Printf("failed to create store: %v", err)
 		return 1
 	}
-	s := newServer(*st, httpPort, cancel, initializeLogger)
+	s := newServer(*st, httpPort, cancel, logger)
 	var serverErr error
 	go func() {
 		serverErr = s.start()
@@ -65,14 +58,42 @@ func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir s
 	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	initializeLogger.Println("Linko is shutting down")
+	logger.Println("Linko is shutting down")
 	if err := s.shutdown(shutdownCtx); err != nil {
-		initializeLogger.Printf("failed to shutdown server: %v", err)
+		logger.Printf("failed to shutdown server: %v", err)
 		return 1
 	}
 	if serverErr != nil {
-		initializeLogger.Printf("server error: %v", serverErr)
+		logger.Printf("server error: %v", serverErr)
 		return 1
 	}
 	return 0
+}
+
+type closeFunc func() error
+
+func initializeLogger(logFile string) (*log.Logger, closeFunc, error) {
+	if logFile != "" {
+		file, err := os.OpenFile(logFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+		if err != nil {
+
+			return nil, nil, fmt.Errorf("Failed to open log file: %w", err)
+		}
+
+		bufferedFile := bufio.NewWriterSize(file, 8192)
+		multiWriter := io.MultiWriter(bufferedFile, os.Stderr)
+		close := func() error {
+			if err := bufferedFile.Flush(); err != nil {
+				return fmt.Errorf("Failed to flush log file: %w", err)
+			}
+			if err := file.Close(); err != nil {
+				return fmt.Errorf("Failed to close log file: %w", err)
+			}
+			return nil
+		}
+		return log.New(multiWriter, "", log.LstdFlags), close, nil
+	}
+
+	return log.New(os.Stderr, "", log.LstdFlags), func() error { return nil }, nil
+
 }
